d6c716af6e353a2fadd571a5c24013437c205d9c
emostarxd <emostarxd@gmail.com>
2016-12-20 16:18:30 -0500

    Restore sql.gz dumps and import users to mysql

--- myrestore.sh
+++ myrestore.sh
@@ -7,27 +7,51 @@
 
 ###DISCLAIMER: in case of fire - steal, kill, fuck the geese, wait for a dial tone response
 
-prepare() {
-list=$(ls -l | grep sql.gz | awk '{print $9}')
-echo "$list"
+prepare() { #get list of database dumps
+	list=$(ls -l | grep sql.gz | awk '{print $9}')
 }
-
-action() {
 prepare
-for db in "$list"
-do
-	# sanitize BASE.sql.gz to BASE
-	dbname=$(echo $db | sed 's/\.sql.gz//')
-	# dump each database in a separate file
-	zcat $db | mysql $dbname
-  
-done
-}
-action
 
-#if [[ -f MySQLUserGrants.sql ]]; then
-#	mysqldump -A < MySQLUserGrants.sql
-#	mysql -e 'FLUSH PRIVILEGES;'
-#else
-#exit 0
-#fi
+action() { #run import if confirmed by user
+
+	for db in "$list"
+	do
+		# sanitize BASE.sql.gz to BASE
+			dbname=$(echo $db | sed 's/\.sql.gz//')
+		# create base in mysql if not exist
+			mysql -e "CREATE DATABASE IF NOT EXISTS $dbname;"
+		# restore each database in a separate file
+			zcat $db | mysql $dbname
+	done
+}
+
+echo "The following databases will be created if not exist, data will be overwritten:"
+echo "$list"
+read -p "Continue? (y/N): " CONFIRM_INPUT
+while true
+do
+	case $CONFIRM_INPUT in
+		[yY]* ) action;;
+		[nN]* ) exit;;
+		* ) echo "Dude, just enter Y or N, please."; break ;;
+	esac
+done
+
+importusers() {
+	mysql < MySQLUserGrants.sql
+	mysql -e 'FLUSH PRIVILEGES;'
+}
+
+if [ -f MySQLUserGrants.sql ]; then
+	read -p "File with users and grants is detected. Continue? (y/N): " CONFIRM_INPUT
+	while true
+	do
+		case $CONFIRM_INPUT in
+			[yY]* ) importusers;;
+			[nN]* ) exit;;
+			* ) echo "Dude, just enter Y or N, please."; break ;;
+		esac
+	done
+else
+	exit 0
+fi
